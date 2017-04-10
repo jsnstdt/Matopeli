7cd8888f8c04201e3cef64415d4d227a51727835
jsnstdt <jan-sebastian.nystedt@student.kpedu.fi>
2017-04-10 09:34:59 +0300

    Ensimmäinen commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	Vector2 dir = Vector2.right;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("Move", 1.0f, 0.3f);
+	}
+	
+	void Move() {
+		transform.Translate (dir);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}

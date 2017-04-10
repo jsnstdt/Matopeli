ac06c38dad373d6f0b3243deaef37b66234b2044
jsnstdt <jan-sebastian.nystedt@student.kpedu.fi>
2017-04-10 09:56:12 +0300

    Koodia

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,19 +4,50 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	Vector2 dir = Vector2.right;
+	public List<GameObject> parts; // Lista madon osista.
+	public GameObject head; // Madon pää
+	private Vector2 dir; // Madon suunta
+	private float speedMultiplier; // Madon nopeus
+	private BoxCollider2D cldr; // BoxCollider2D jolla tarkistetaan osuuko mato johonkin omaan osaansa
+								// tai kentän reunaan.
 
-	// Use this for initialization
 	void Start () {
-		InvokeRepeating ("Move", 1.0f, 0.3f);
+		cldr = GetComponent<BoxCollider2D> ();
+		parts.Add (head);
+		speedMultiplier = 1.0f;
+		dir = Vector2.right; // Liikutaan aluksi oikealle.
+		// Liikutetaan matoa sekunnin jälkeen tietyin aikavälein.
+		// Mato liikkuu aluksi 0.3 sekunnin välein, mutta nopeutuu kun pisteet kasvavat.
+		InvokeRepeating ("Move", 1.0f, 0.3f * speedMultiplier);
 	}
 	
 	void Move() {
+		// Liikutetaan pelaajaa tietyn määrän. Fysiikkamoottoria ei tarvitse käyttää
+		// yksinkertaisessa matopelissä.
 		transform.Translate (dir);
 	}
 
 	// Update is called once per frame
 	void Update () {
 
-	}
+		// Tarkistetaan yrittääkö pelaaja liikkua johonkin suuntaan.
+		float horiz = Input.GetAxis ("Horizontal");
+		float verti = Input.GetAxis ("Vertical");
+
+		// Vaihdetaan mahdollisesti suuntaa, ja tarkistetaan että pelaaja pystyy varmasti
+		// kääntyä haluttuun suuntaan.
+
+		if (horiz != 0) {
+			if (dir != Vector2.right && horiz < 0)
+				dir = Vector2.left;
+			else if (dir != Vector2.left && horiz > 0)
+				dir = Vector2.right;
+		}
+		if (verti != 0) {
+			if (dir != Vector2.up && verti < 0)
+				dir = Vector2.down;
+			else if (dir != Vector2.down && verti > 0)
+				dir = Vector2.up;
+		}
+}
 }
